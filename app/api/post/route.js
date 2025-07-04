6722b1aae6d69d51d0af8a5c847612420dcb424e
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-07-05 00:27:20 +0300

    feat: add Post model to Prisma schema and configure MongoDB datasource
    chore: add PostCSS configuration for Tailwind CSS
    
    feat: add SVG icons for various UI elements including file, globe, next, vercel, and window

---
+++ app/api/post/route.js
@@ -0,0 +1,24 @@
+export const POST = async (request) => {
+  try {
+    const body = await request.json();
+    const { title, description } = body;
+
+    const newPost = await Prisma.post.create({
+      data: {
+        title,
+        description,
+      },
+    });
+    return NextResponse.json(newPost);
+  } catch (error) {
+    return NextResponse.json({ message: "POST Error", error }, { status: 500 });
+  }
+};
+export const GET= async(request)=>{
+    try{
+ const posts= await Prisma.post.findMany()
+ return NextResponse.json(posts)
+    }catch(error){
+return NextResponse.json({message:"GET Error", error}, {status:500})
+    }
+}
---
+++ libs/prismadb.js
@@ -0,0 +1,8 @@
+import { Prisma } from "@/app/generated/prisma";
+import {PrismaClient} from "@prisma/client";
+
+const client= globalThis.prisma || new PrismaClient();
+if(process.env.NODE_ENV !== "production") {
+  globalThis.prisma = client;
+}
+export default client;
